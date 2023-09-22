8c2891c8fce811a8d1d558e8df8c07baf9dfb1fd
JelleLagerweij <v.j.lagerweij@tudelft.nl>
2023-09-22 10:52:55 +0200

    small change to switch to cheaper file

--- post-processing scripts/KOH systems/compute.py
+++ post-processing scripts/KOH systems/compute.py
@@ -48,68 +48,68 @@ marker = ['o', 'x', '^', '>']
 # # Done
 
 # Presentations
-pgf_with_latex = {                      # setup matplotlib to use latex for output
-    "pgf.texsystem": "lualatex",        # change this if using xetex or lautex
-    "text.usetex": True,                # use LaTeX to write all text
-    "font.family": 'serif',
-    "pgf.rcfonts": False,    # don't setup fonts from rc parameters
-    "pgf.preamble": "\n".join([ # plots will use this preamble
-        r"\RequirePackage{amsmath}",
-        r"\RequirePackage{fontspec}",   # unicode math setup
-        r"\setmainfont[Scale = MatchLowercase]{DejaVu Serif}",
-        r"\setsansfont[Scale = MatchLowercase]{DejaVu Sans}",
-        r"\setmonofont[Scale = MatchLowercase]{DejaVu Sans Mono}",
-        r"\usepackage{unicode-math}",
-        r"\setmathfont[Scale = MatchLowercase]{DejaVu Math TeX Gyre}", 
-        r"\usepackage{siunitx}",
-        r"\usepackage[version=3]{mhchem}"])}
+# pgf_with_latex = {                      # setup matplotlib to use latex for output
+#     "pgf.texsystem": "lualatex",        # change this if using xetex or lautex
+#     "text.usetex": True,                # use LaTeX to write all text
+#     "font.family": 'serif',
+#     "pgf.rcfonts": False,    # don't setup fonts from rc parameters
+#     "pgf.preamble": "\n".join([ # plots will use this preamble
+#         r"\RequirePackage{amsmath}",
+#         r"\RequirePackage{fontspec}",   # unicode math setup
+#         r"\setmainfont[Scale = MatchLowercase]{DejaVu Serif}",
+#         r"\setsansfont[Scale = MatchLowercase]{DejaVu Sans}",
+#         r"\setmonofont[Scale = MatchLowercase]{DejaVu Sans Mono}",
+#         r"\usepackage{unicode-math}",
+#         r"\setmathfont[Scale = MatchLowercase]{DejaVu Math TeX Gyre}", 
+#         r"\usepackage{siunitx}",
+#         r"\usepackage[version=3]{mhchem}"])}
 
-mpl.use("pgf")
-mpl.rcParams.update(pgf_with_latex)
-plt.style.use('dark_background')
+# mpl.use("pgf")
+# mpl.rcParams.update(pgf_with_latex)
+# plt.style.use('dark_background')
 
-plt.rcParams["axes.grid"] = "False"
+# plt.rcParams["axes.grid"] = "False"
 
-# Sizes of specific parts
-plt.rcParams['lines.markersize'] = 10
-plt.rcParams['lines.markeredgewidth'] = 2
-plt.rcParams['lines.linewidth'] = 2
+# # Sizes of specific parts
+# plt.rcParams['lines.markersize'] = 10
+# plt.rcParams['lines.markeredgewidth'] = 2
+# plt.rcParams['lines.linewidth'] = 2
 
-plt.rcParams['axes.linewidth'] = 2
-plt.rcParams['axes.labelsize']= 0.75
-plt.rcParams['xtick.major.pad'] = 7
-plt.rcParams['ytick.major.pad'] = 7
+# plt.rcParams['axes.linewidth'] = 2
+# plt.rcParams['axes.labelsize']= 0.75
+# plt.rcParams['xtick.major.pad'] = 7
+# plt.rcParams['ytick.major.pad'] = 7
 
-plt.rcParams['xtick.major.size'] = 7
-plt.rcParams['xtick.major.width'] = 2
-plt.rcParams['xtick.direction'] =  'in'
-plt.rcParams['xtick.top'] = True
-plt.rcParams['ytick.major.size'] = 7
-plt.rcParams['ytick.major.width'] = 2
-plt.rcParams['ytick.direction'] =  'in'
-plt.rcParams['ytick.right'] = True
+# plt.rcParams['xtick.major.size'] = 7
+# plt.rcParams['xtick.major.width'] = 2
+# plt.rcParams['xtick.direction'] =  'in'
+# plt.rcParams['xtick.top'] = True
+# plt.rcParams['ytick.major.size'] = 7
+# plt.rcParams['ytick.major.width'] = 2
+# plt.rcParams['ytick.direction'] =  'in'
+# plt.rcParams['ytick.right'] = True
 
-plt.rcParams["legend.frameon"] = False
-plt.rcParams['font.size'] = 18
-plt.rcParams['axes.labelpad']= 7
-plt.rcParams['xtick.labelsize'] = 13
-plt.rcParams['ytick.labelsize'] = 13
-plt.rcParams['legend.fontsize'] = 15
+# plt.rcParams["legend.frameon"] = False
+# plt.rcParams['font.size'] = 18
+# plt.rcParams['axes.labelpad']= 7
+# plt.rcParams['xtick.labelsize'] = 13
+# plt.rcParams['ytick.labelsize'] = 13
+# plt.rcParams['legend.fontsize'] = 15
 
-# figures = r'C:\Users\Jelle\Delft University of Technology\Jelle Lagerweij Master - Documents\General\Personal Thesis files\01 Defence Presentation\Figures'
+# # figures = r'C:\Users\Jelle\Delft University of Technology\Jelle Lagerweij Master - Documents\General\Personal Thesis files\01 Defence Presentation\Figures'
 figures = r'C:\Users\Jelle\Documents\TU jaar 6\KOH (aq) Project\Progress_meeting_2\figures'
-plt.rcParams['savefig.directory'] = figures
+# plt.rcParams['savefig.directory'] = figures
 # Done
 
 ###############################################################################
 
 ###############################################################################
 
-path = '../../../RPBE_Production/MLMD/100ps_Exp_Density/'
+# path = '../../../RPBE_Production/MLMD/100ps_Exp_Density/'
 # path = '../../../RPBE_Production/MLMD/100ps_2/'
-# path = '../../../RPBE_Production/AIMD/10ps/'
+path = '../../../RPBE_Production/AIMD/10ps/'
 # folder = ['i_1', 'i_2', 'i_3', 'i_4', 'i_5']
-folder = ['i_5']
+folder = ['i_1']
 
 n_KOH = 1
 n_H2O = 55
