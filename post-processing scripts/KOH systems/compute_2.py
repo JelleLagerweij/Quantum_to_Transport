3d30941e244bf719b2ea01f2dd9ba1a7a594569e
JelleLagerweij <v.j.lagerweij@tudelft.nl>
2023-09-29 13:53:10 +0200

    done

--- post-processing scripts/KOH systems/compute_2.py
+++ post-processing scripts/KOH systems/compute_2.py
@@ -142,55 +142,55 @@ for i in range(len(folder)):
     hists_s += hist/len(folder)
     
 
-    # visc[i, :] = Traj.viscosity(cubicspline=10, plotting=True, padding=0)
+    visc[i, :] = Traj.viscosity(cubicspline=10, plotting=True, padding=0)
 
-    # r, rdfs, n_conf = Traj.rdf(interpol=64, plotting=False,
-    #                            r_end=[4.2, 3.3, 3.5])
-    # n_OO[i], n_HO[i], n_KO[i] = n_conf
-    # OO[i, :], HO[i, :], KO[i, :] = rdfs
+    r, rdfs, n_conf = Traj.rdf(interpol=64, plotting=False,
+                               r_end=[4.2, 3.3, 3.5])
+    n_OO[i], n_HO[i], n_KO[i] = n_conf
+    OO[i, :], HO[i, :], KO[i, :] = rdfs
 
-    # d_OO[i] = r[np.argmax(OO[i, :])]
-    # d_HO[i] = r[np.argmax(HO[i, :])]
-    # d_KO[i] = r[np.argmax(KO[i, :])]
+    d_OO[i] = r[np.argmax(OO[i, :])]
+    d_HO[i] = r[np.argmax(HO[i, :])]
+    d_KO[i] = r[np.argmax(KO[i, :])]
 
-    # # sanity checks
-    # # ave_beef, ave_bees = Traj.bayes_error(plotting=True, move_ave=2500)
-    # press[i, :] = Traj.pressure(plotting=True, filter_width=2500, skip=100)
-    # e_kin[i, :] = Traj.kin_energy(plotting=True, filter_width=2500, skip=100)
-    # e_pot[i, :] = Traj.pot_energy(plotting=True, filter_width=2500, skip=100)
-    # e_tot[i, :] = Traj.tot_energy(plotting=True, filter_width=2500, skip=100)
-    # Temp[i, :] = Traj.temperature(plotting=True, filter_width=2500, skip=100)
+    # sanity checks
+    # ave_beef, ave_bees = Traj.bayes_error(plotting=True, move_ave=2500)
+    press[i, :] = Traj.pressure(plotting=True, filter_width=2500, skip=100)
+    e_kin[i, :] = Traj.kin_energy(plotting=True, filter_width=2500, skip=100)
+    e_pot[i, :] = Traj.pot_energy(plotting=True, filter_width=2500, skip=100)
+    e_tot[i, :] = Traj.tot_energy(plotting=True, filter_width=2500, skip=100)
+    Temp[i, :] = Traj.temperature(plotting=True, filter_width=2500, skip=100)
 
-    # msdOH = Traj.windowed_MSD(loc_OH, n_KOH)
-    # msdK = Traj.windowed_MSD(loc_K, n_KOH)
-    # msdH2O = Traj.windowed_MSD(loc_H2O, n_H2O)
+    msdOH = Traj.windowed_MSD(loc_OH, n_KOH)
+    msdK = Traj.windowed_MSD(loc_K, n_KOH)
+    msdH2O = Traj.windowed_MSD(loc_H2O, n_H2O)
 
-    # # t = np.arange(len(Traj.t))
+    # t = np.arange(len(Traj.t))
 
-    # # multiple window loglog
-    # plt.figure('multiple window loglog OH')
-    # plt.loglog(Traj.t[1:]*1e12, msdOH[1:], label=folder[i])
+    # multiple window loglog
+    plt.figure('multiple window loglog OH')
+    plt.loglog(Traj.t[1:]*1e12, msdOH[1:], label=folder[i])
 
-    # # multiple window loglog
-    # plt.figure('multiple window loglog K')
-    # plt.loglog(Traj.t[1:]*1e12, msdK[1:], label=folder[i])
+    # multiple window loglog
+    plt.figure('multiple window loglog K')
+    plt.loglog(Traj.t[1:]*1e12, msdK[1:], label=folder[i])
 
-    # # multiple window loglog
-    # plt.figure('multiple window loglog H2O')
-    # plt.loglog(Traj.t[1:]*1e12, msdH2O[1:], label=folder[i])
+    # multiple window loglog
+    plt.figure('multiple window loglog H2O')
+    plt.loglog(Traj.t[1:]*1e12, msdH2O[1:], label=folder[i])
 
-    # plt.figure('OH index')
-    # plt.plot(Traj.t*1e12, index - Traj.N_H, label=folder[i])
+    plt.figure('OH index')
+    plt.plot(Traj.t*1e12, index - Traj.N_H, label=folder[i])
 
-    # n = np.arange(start=1000, stop=len(Traj.t), step=100)
-    # n = np.arange(start=10000, stop=len(Traj.t), step=100)
-    # t_test = Traj.t[n]
-    # MSD_test = msdOH[n]
-    # diff_OH[i] = Traj.diffusion(MSD_test, n_KOH, t=t_test, plotting=True)
-    # MSD_test = msdK[n]
-    # diff_K[i] = Traj.diffusion(MSD_test, n_KOH, t=t_test, plotting=True)
-    # MSD_test = msdH2O[n]
-    # diff_H2O[i] = Traj.diffusion(MSD_test, n_H2O, t=t_test, plotting=True)
+    n = np.arange(start=1000, stop=len(Traj.t), step=100)
+    n = np.arange(start=10000, stop=len(Traj.t), step=100)
+    t_test = Traj.t[n]
+    MSD_test = msdOH[n]
+    diff_OH[i] = Traj.diffusion(MSD_test, n_KOH, t=t_test, plotting=True)
+    MSD_test = msdK[n]
+    diff_K[i] = Traj.diffusion(MSD_test, n_KOH, t=t_test, plotting=True)
+    MSD_test = msdH2O[n]
+    diff_H2O[i] = Traj.diffusion(MSD_test, n_H2O, t=t_test, plotting=True)
 
 #     plt.figure('Number OH-')
 #     plt.plot(Traj.t*1e12, Traj.N_OH, label=folder[i])
