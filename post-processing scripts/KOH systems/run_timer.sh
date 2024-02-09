4115a3524eadc4907152fa6cb0dcfbb71795b0df
JelleLagerweij <v.j.lagerweij@tudelft.nl>
2024-02-09 17:14:01 +0100

    lozl parallel efficency tested

--- post-processing scripts/KOH systems/run_timer.sh
+++ post-processing scripts/KOH systems/run_timer.sh
@@ -19,7 +19,7 @@
 # module load miniconda3
 # conda activate vasp_post
 
-conda env list
+# conda env list
 # echo "16 HT START"
 # mpiexec -n 16 --use-hwthread-cpus python mpi_python.py
 # echo "16 HT COMPLETED"
